985198a6eb7b2704f4da55547d949b2e86ae44cb
Sean Grove <sean@fakecoolguys.com>
2010-10-25 12:05:44 -0700

    Fixed some of the async stuff, but don't know how to do retry yet.

--- wikiparanode.js
+++ wikiparanode.js
@@ -5,43 +5,36 @@ var request = require('request')
 // Will this 1337 hax0rs get me into the Neck Beard Society?
 var chromeUserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.X.Y.Z Safari/525.13.";
 
-var getText = function(minWordCount) {
+var getText = function(minWordCount, cb) {
     minWordCount = minWordCount || 10;
+
+    var requestHandler = function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var window = jsdom.jsdom(body).createWindow();
+            jsdom.jQueryify(window, 'lib/jquery-1.4.2.min.js', function (window, $) {
+                // jQuery is now loaded on the jsdom window created from 'body'
+                var paragraphs = $("#content p");
+                var index = Math.round(Math.random() * paragraphs.length);
+                var paragraph = $(paragraphs[index]);
+                
+                cb(paragraph.text());
+            });
+        } else {
+            sys.puts("Error retrieving url [" + response.statusCode + "]");
+        }
+    };
+
 
     var worker = function() {
         request({
             uri:'http://en.wikipedia.org/wiki/Special:Random',
             headers: {"User-Agent": chromeUserAgent}
-        },
-                function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        var window = jsdom.jsdom(body).createWindow();
-                        jsdom.jQueryify(window, 'lib/jquery-1.4.2.min.js', function (window, $) {
-                            // jQuery is now loaded on the jsdom window created from 'body'
-                            var paragraphs = $("#content p");
-                            var index = Math.round(Math.random() * paragraphs.length);
-                            var paragraph = $(paragraphs[index]);
-                            
-                            cb(paragraph.text());
-                        });
-                    } else {
-                        sys.puts("Error retrieving url [" + response.statusCode + "]");
-                    }
-                });
+        }, requestHandler)
     };
 
-    var text = worker();
-
-    if (text.split(" ").length >= minWordCount) {
-        return text;
-    } else {
-        getText(minWordCount);
-    }
+    // TODO: How to check for minWordCount?
+    return worker();
 }
 
 
-p1 = getText(10);
-p2 = getText(50);
-
-console.log(p1.text());
-console.log(p2.text());
+getText(10, function(result) { console.log(result); });
