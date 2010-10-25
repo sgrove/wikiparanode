e27263e6b83f4fe65bb8b71628d9e1491789b3be
Sean Grove <sean@fakecoolguys.com>
2010-10-25 11:33:20 -0700

    Async problems. Always run into this. But that's why we keep doing these mini-problems before a big project.

--- wikiparanode.js
+++ wikiparanode.js
@@ -5,23 +5,43 @@ var request = require('request')
 // Will this 1337 hax0rs get me into the Neck Beard Society?
 var chromeUserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.X.Y.Z Safari/525.13.";
 
-request({
-    uri:'http://en.wikipedia.org/wiki/Special:Random',
-    headers: {"User-Agent": chromeUserAgent}
-},
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var window = jsdom.jsdom(body).createWindow();
-                jsdom.jQueryify(window, 'lib/jquery-1.4.2.min.js', function (window, $) {
-                    // jQuery is now loaded on the jsdom window created from 'body'
-                    var paragraphs = $("#content p");
-                    var index = Math.round(Math.random() * paragraphs.length);
-                    var paragraph = $(paragraphs[index]);
-                    
-                    console.log(paragraph.text());
-                    $('.someClass').each(function () { /* Your custom logic */ });
+var getText = function(minWordCount) {
+    minWordCount = minWordCount || 10;
+
+    var worker = function() {
+        request({
+            uri:'http://en.wikipedia.org/wiki/Special:Random',
+            headers: {"User-Agent": chromeUserAgent}
+        },
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var window = jsdom.jsdom(body).createWindow();
+                        jsdom.jQueryify(window, 'lib/jquery-1.4.2.min.js', function (window, $) {
+                            // jQuery is now loaded on the jsdom window created from 'body'
+                            var paragraphs = $("#content p");
+                            var index = Math.round(Math.random() * paragraphs.length);
+                            var paragraph = $(paragraphs[index]);
+                            
+                            cb(paragraph.text());
+                        });
+                    } else {
+                        sys.puts("Error retrieving url [" + response.statusCode + "]");
+                    }
                 });
-            } else {
-                sys.puts("Error retrieving url [" + response.statusCode + "]");
-            }
-        });
+    };
+
+    var text = worker();
+
+    if (text.split(" ").length >= minWordCount) {
+        return text;
+    } else {
+        getText(minWordCount);
+    }
+}
+
+
+p1 = getText(10);
+p2 = getText(50);
+
+console.log(p1.text());
+console.log(p2.text());
